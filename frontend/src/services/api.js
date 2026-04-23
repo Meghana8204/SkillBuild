272b9f0aec56c0f06beaf5d4f51240d9de200ce2
Meghana8204 <meghanaar222@gmail.com>
2026-04-23 19:27:19 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://skillbuild-2.onrender.com/api",
+  baseURL: "https://skillbuild-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
